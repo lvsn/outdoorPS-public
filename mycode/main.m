8dea94c06e4320f1c586438576cb3aa82aab5703
Jinsong <jacenfox@gmail.com>
2015-04-16 21:25:07 +0800

    Select the image base on time-stamps, do not use findMatrixA.m anymore

--- mycode/main.m
+++ mycode/main.m
@@ -15,19 +15,21 @@ dateValue = '20141108'; % '20141108'
 % list the raw data
 X = getfilenames(fullfile(databasePath,dateValue),'envmap.exr',1,1);
 
-% resize the raw image 
-MAPSIZE = 256;
-
-% preparing for computing
-nIms = size(X,2);
-matA = zeros(nIms,3);
-
 % sample the directions
 sphereSize = 3; % 642 vertices
 tri = SubdivideSphericalMesh(IcosahedronMesh, sphereSize);
 tmp_x = tri.X(:,1)';tmp_y = tri.X(:,2)';tmp_z = tri.X(:,3)';
 normal_fullSphere = cat(1,row(tmp_y),row(tmp_z),row(tmp_x));
 clear tmp_*
+
+% resize the raw image 
+MAPSIZE = 256;
+
+% preparing for computing
+nIms = size(X,2); 
+time_interval_a = '10:30:00';
+time_interval_b = '16:30:00';
+r = 0;
 
 assert(nIms > 0, 'No environment map in the specified folder');
 disp('Computing the mean light vectors for a day');
@@ -36,7 +38,14 @@ for i_x = 1:nIms
     if mod(i_x,10)==1 && nIms > 30
         fprintf('  computing mean light vector for EnvMap: %d in %d\n', i_x,nIms);
     end
+
+    % choose the image base on time interval
+    if datestr(datenum(datestr(e.dateNumber,'HHMMSS'),'HHMMSS') - datenum(time_interval_a,'HH:MM:SS'),'HHMMSS') <=0 || ...
+       datestr(datenum(datestr(e.dateNumber,'HHMMSS'),'HHMMSS') - datenum(time_interval_b,'HH:MM:SS'),'HHMMSS') >=0 
+        continue;
+    end
     
+    % load the environment map
 	e = EnvironmentMap(X{i_x});
 	e = imresize(e, [MAPSIZE, MAPSIZE]);
 
@@ -44,8 +53,9 @@ for i_x = 1:nIms
     [matA_fullSphere,b_ground] = findAi(e, normal_fullSphere);
     
     % save the result to a structure
-    matA.fullSphere(i_x,:,:) = matA_fullSphere;
-    matA.b_ground(i_x,:,:) = b_ground(1,1,:);    
+    r = r + 1;
+    matA.fullSphere(r,:,:) = matA_fullSphere;
+    matA.b_ground(r,:,:) = b_ground(1,1,:);
 end
 
 % orginaze the result
@@ -57,19 +67,6 @@ matA.info.imageSize = MAPSIZE;
 % save the structure
 % resultFilename = fullfile(resultbasePath, sprintf('%s_matA.mat',dateValue));
 % save(resultFilename,'matA');
-
-% TODO: We do not have the interval data, try to replace the function
-% findMatrixA.m by a simpler method.
-% load time interval data
-interval = 6;
-load(sprintf('data/intervalData/data_%d.mat',interval));
-load('data/intervalData/db.mat');
-
-% constructs the illumination matrix
-matA = findMatrixA(matA, lightDb, lightInd, d);
-if isempty(matA)
-    disp('no valid data in this day'); return;
-end
 
 % compute the confidence interval
 noise =  0.01;
