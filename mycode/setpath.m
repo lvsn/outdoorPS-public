213737054bfaecad81bda4a7c0260c6ad92c9291
Jinsong <jacenfox@gmail.com>
2015-04-20 12:09:04 +0800

    set path for the requirements code

---
+++ mycode/setpath.m
@@ -0,0 +1,11 @@
+% add the 3rd_party
+addpath(genpath('./3rd_party/Uniform_Sampling_of_S2'));
+
+% assert(exist('../utils/mycode','file')>0,'required utils package');
+% assert(exist('../hdrutils/mycode','file')>0,'required hdrutils package');
+% assert(exist('../envMapConversions/mycode','file')>0,'required envMapConversions package');
+
+addpath(genpath('../utils'));
+addpath(genpath('../hdrutils'));
+addpath(genpath('../envMapConversions'));
+
